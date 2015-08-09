360a81244e448685ac8439877f8ae4445b800d4b
reyley <reyley1014@gmail.com>
2015-08-09 23:09:32 +0300

    trying to get the server to work

--- Character_Generator/settings.py
+++ Character_Generator/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mod_wsgi.server',
+    'django.core.wsgi',
     'Character_Generator'
 )
 
