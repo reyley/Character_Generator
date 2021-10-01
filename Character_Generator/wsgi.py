9c092e902aee1d695e78a1a099dbd54f67481be8
Code Carefully <CodeCarefully@users.noreply.github.com>
2021-10-01 03:15:09 +0300

    Updated to Django 3.x

--- Character_Generator/wsgi.py
+++ Character_Generator/wsgi.py
@@ -4,11 +4,13 @@ WSGI config for Character_Generator project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Character_Generator.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Character_Generator.settings')
+
 application = get_wsgi_application()
